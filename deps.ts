21e3f4f0407041ec57ebcbb4348c4af519cb45b0
jia wei <jw397@126.com>
2021-11-05 13:46:01 +0800

    feat: 非deno调用的直接转发

---
+++ deps.ts
@@ -0,0 +1,17 @@
+export { contentType } from "https://deno.land/x/mime_types@1.0.0/mod.ts";
+
+export {
+  bgBlue,
+  bgRgb24,
+  bgRgb8,
+  blue,
+  bold,
+  green,
+  italic,
+  red,
+  rgb24,
+  rgb8,
+  yellow,
+} from "https://deno.land/std@0.97.0/fmt/colors.ts";
+
+export { parse } from "https://deno.land/std@0.97.0/flags/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,68 +1,95 @@
-// const url = `https://deno.land/x/oak@v9.0.1/deps.ts`;
-import { contentType } from "https://deno.land/x/mime_types@1.0.0/mod.ts";
+import { contentType, parse } from "./deps.ts";
+
+const CACHE_DIR = "data";
+
+async function readTextFile(path: string) {
+  try {
+    const text = await Deno.readTextFile(path);
+    return text;
+  } catch {
+    return null;
+  }
+}
 
 async function fetchFromRemote(url: string) {
+  const filePath = CACHE_DIR + "/" + url.replace(/:\/\//, "/");
+  const text = await readTextFile(filePath);
+  if (text !== null) {
+    console.debug(`${url} loaded from local file`);
+    return text;
+  }
   const res = await fetch(url);
-  const data = await res.text();
-  const dir = "data/" + url.replace(/:\/\//, "/");
-  const arr = dir.split("/");
+  const data = await res.arrayBuffer();
+  console.info(`${url} loaded from remote file ok`);
+  const arr = filePath.split("/");
   arr.pop();
   await Deno.mkdir(arr.join("/"), { recursive: true });
-  await Deno.writeTextFile(dir, data);
+  await Deno.writeFile(filePath, new Uint8Array(data));
   return data;
 }
 
-// fetchFromRemote(url);
-
-// Start listening on port 8080 of localhost.
-const server = Deno.listen({ port: 8080 });
-console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
-
-// Connections to the server will be yielded up as an async iterable.
-for await (const conn of server) {
-  // In order to not be blocking, we need to handle each connection individually
-  // without awaiting the function
-  serveHttp(conn);
-}
-
 async function serveHttp(conn: Deno.Conn) {
-  // This "upgrades" a network connection into an HTTP connection.
   const httpConn = Deno.serveHttp(conn);
-  // Each request sent over the HTTP connection will be yielded as an async
-  // iterator from the HTTP connection.
   for await (const requestEvent of httpConn) {
-    console.log(requestEvent.request.url);
-    const url = requestEvent.request.url;
-    if (!url.endsWith(".ts") && !url.endsWith(".js")) {
+    const req = requestEvent.request;
+    const url = req.url;
+    console.info(`${req.method} ${url}`);
+    const urlMap = new URL(url);
+    const tempArr = urlMap.pathname.split("/").filter(Boolean);
+    const first = tempArr.shift()!;
+    if (!/^http[s]$/.test(first)) {
       return requestEvent.respondWith(
-        new Response("Not Supported", {
+        new Response("Wrong agreement", {
           status: 404,
         }),
       );
     }
-    // The native HTTP server uses the web standard `Request` and `Response`
-    // objects.
-    // const body = `Your user-agent is:\n\n${
-    //   requestEvent.request.headers.get(
-    //     "user-agent",
-    //   ) ?? "Unknown"
-    // }`;
-
-    const urlMap = new URL(url);
-    console.log(urlMap);
-    const remoteUrl = "https://deno.land" + urlMap.pathname;
-    console.log(remoteUrl);
-    const body = await fetchFromRemote(remoteUrl);
-    // const body = "ok";
-
-    // The requestEvent's .respondWith() method is how we send the response back to the client.
-    requestEvent.respondWith(
-      new Response(body, {
-        status: 200,
-        headers: new Headers({
-          "content-type": contentType(url) || "text/plain",
+    const realUrl = first + "://" + tempArr.join("/");
+    const userAgent = req.headers.get("User-Agent");
+    if (!userAgent || !userAgent.startsWith("Deno")) {
+      return requestEvent.respondWith(
+        new Response(null, {
+          status: 301,
+          headers: new Headers({
+            location: realUrl,
+          }),
         }),
-      }),
-    );
+      );
+    }
+    try {
+      const body = await fetchFromRemote(realUrl);
+      requestEvent.respondWith(
+        new Response(body, {
+          status: 200,
+          headers: new Headers({
+            "content-type": contentType(url) || "text/plain",
+          }),
+        }),
+      );
+    } catch (e) {
+      requestEvent.respondWith(
+        new Response(e.message, {
+          status: 500,
+        }),
+      );
+    }
   }
 }
+
+interface ServerArgs {
+  port?: string;
+}
+
+async function main() {
+  const serverArgs = parse(Deno.args) as ServerArgs;
+  const port = serverArgs.port || Deno.env.get("PORT") || "8080";
+  const server = Deno.listen({ port: Number(port) });
+  console.log(
+    `HTTP webserver running.  Access it at:  http://localhost:${port}/`,
+  );
+  for await (const conn of server) {
+    serveHttp(conn);
+  }
+}
+
+main();
